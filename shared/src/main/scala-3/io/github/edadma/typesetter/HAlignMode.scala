9939ba451a9e0cceab42e927f7aa21bd062ccd37
Edward A Maxedon <edadma@gmail.com>
2024-10-28 19:36:39 -0400

    HAlignMode

--- shared/src/main/scala-3/io/github/edadma/typesetter/HAlignMode.scala
+++ shared/src/main/scala-3/io/github/edadma/typesetter/HAlignMode.scala
@@ -5,22 +5,34 @@ import scala.collection.mutable.{ArrayBuffer, ListBuffer}
 class HAlignMode(val t: Typesetter) extends Mode:
   private var state: "FORMAT" | "CONTENT" = "FORMAT"
 
-  val format  = new ArrayBuffer[ListBuffer[Box]]
-  val content = new ArrayBuffer[ArrayBuffer[ListBuffer[Box]]]
+  val format             = new ArrayBuffer[ListBuffer[Box]]
+  var formatColumns: Int = 0
+  val content            = new ArrayBuffer[ArrayBuffer[ListBuffer[Box]]]
+  var lineColumns: Int   = 0
 
-  private def newColumn(): Unit =
+  def newColumn(): Unit =
     state match
-      case "FORMAT"  => format += new ListBuffer
-      case "CONTENT" => content.last += new ListBuffer
-
-  private def newLine(): Unit =
-    state match
-      case "FORMAT"  => state = "CONTENT"
+      case "FORMAT" =>
+        format += new ListBuffer
+        formatColumns += 1
       case "CONTENT" =>
+        content.last += new ListBuffer
+        lineColumns += 1
+        if lineColumns > formatColumns then sys.error("too many columns")
+
+  def newLine(): Unit =
+    state match
+      case "FORMAT" =>
+        if formatColumns == 0 then sys.error("need at least one column")
+        state = "CONTENT"
+      case "CONTENT" =>
+        if lineColumns < formatColumns then sys.error("too few columns")
+        lineColumns = 0
 
     content += new ArrayBuffer
+    newColumn()
 
-  def init(): Unit = ()
+  def init(): Unit = newColumn()
 
   def add(box: Box): Unit =
     state match
@@ -28,6 +40,8 @@ class HAlignMode(val t: Typesetter) extends Mode:
       case "CONTENT" => content.last.last += box
 
   override def done(): Unit =
+    val hboxes = new ArrayBuffer[ArrayBuffer[HBoxBuilder]]
+
     super.done()
 
   def result: Box = ???
