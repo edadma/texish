22709c554cd78253f0c64cd15904687d7088c2e7
Edward A Maxedon <edadma@gmail.com>
2024-10-28 19:23:16 -0400

    HAlignMode

--- shared/src/main/scala-3/io/github/edadma/typesetter/HAlignMode.scala
+++ shared/src/main/scala-3/io/github/edadma/typesetter/HAlignMode.scala
@@ -1,34 +1,33 @@
 package io.github.edadma.typesetter
 
-import scala.collection.mutable.ArrayBuffer
+import scala.collection.mutable.{ArrayBuffer, ListBuffer}
 
 class HAlignMode(val t: Typesetter) extends Mode:
   private var state: "FORMAT" | "CONTENT" = "FORMAT"
 
-  val format  = new ArrayBuffer[HBoxBuilder]
-  val content = new ArrayBuffer[ArrayBuffer[HBoxBuilder]]
+  val format  = new ArrayBuffer[ListBuffer[Box]]
+  val content = new ArrayBuffer[ArrayBuffer[ListBuffer[Box]]]
 
   private def newColumn(): Unit =
     state match
-      case "FORMAT"  => format += new HBoxBuilder(t)
-      case "CONTENT" => content.last += new HBoxBuilder(t)
+      case "FORMAT"  => format += new ListBuffer
+      case "CONTENT" => content.last += new ListBuffer
 
   private def newLine(): Unit =
     state match
       case "FORMAT"  => state = "CONTENT"
       case "CONTENT" =>
 
-    content += new ArrayBuffer[HBoxBuilder]
+    content += new ArrayBuffer
 
   def init(): Unit = ()
 
   def add(box: Box): Unit =
     state match
-      case "FORMAT"  => format.last add box
-      case "CONTENT" => content.last.last add box
+      case "FORMAT"  => format.last += box
+      case "CONTENT" => content.last.last += box
 
   override def done(): Unit =
-    
     super.done()
 
   def result: Box = ???
