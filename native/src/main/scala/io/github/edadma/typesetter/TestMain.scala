698cbd7570a50d12c9dc51aa25de5eaadf9e131e
Edward A Maxedon <edadma@gmail.com>
2024-10-29 18:45:29 -0400

    HAlignMode

--- native/src/main/scala/io/github/edadma/typesetter/TestMain.scala
+++ native/src/main/scala/io/github/edadma/typesetter/TestMain.scala
@@ -3,14 +3,23 @@ package io.github.edadma.typesetter
 @main def run(): Unit =
   val t = CairoPDFTypesetter("a.pdf")
 
-  t.start()
-  t add "[ppasdf"
+//  t.start()
+//  t add "[ppasdf"
+//
+//  for _ <- 1 to 20 do
+//    t add " "
+//    t add "[hTz[[["
+//
+//  t.paragraph()
+//  t.fil
 
-  for _ <- 1 to 20 do
-    t add " "
-    t add "[hTz[[["
+  t.halign
+    .add("(").op("placeholder").add(")").add(" ").fil
+    .op("newColumn").add("[").op("placeholder").add("]").fil
+    .op("newLine")
+    .add("asdf").op("newColumn").add("zxcv")
+    .op("newLine")
+    .add("asdf1").op("newColumn").add("zxcv1")
 
-  t.paragraph()
-  t.fil
   t.end()
   t.destroy()
