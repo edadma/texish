e07ed28e67263cecda62bbfa3a4e4a441d49ba82
Edward A Maxedon <edadma@gmail.com>
2025-05-24 14:27:56 -0400

    test A4

--- native/src/main/scala/io/github/edadma/typesetter/TestMain.scala
+++ native/src/main/scala/io/github/edadma/typesetter/TestMain.scala
@@ -1,23 +1,19 @@
 package io.github.edadma.typesetter
 
 @main def run(): Unit =
-  val t = new CairoPDFTypesetter("a.pdf")
+  val t1 = new CairoPDFTypesetter("a4.pdf")
 
-//  t.start()
-//  t add "[ppasdf"
-//
-//  for _ <- 1 to 20 do
-//    t add " "
-//    t add "[hTz[[["
-//
-//  t.paragraph()
-//  t.fil
+  t1.set("paperwidth", 297 * t1.mm)  // A4 width
+  t1.set("paperheight", 210 * t1.mm) // A4 height
+  t1.add("This is A4 size")
+  t1.end()
+  t1.destroy()
 
-  t.add("asdf")
-  t.selectFont("gentium", 14, Set("regular"))
-  t.add("asdf")
-  t.selectFont("noto", 14, Set("regular"))
-  t.add("asdf")
+  // Test Letter size
+  val t2 = new CairoPDFTypesetter("letter.pdf")
 
-  t.end()
-  t.destroy()
+  t2.set("paperwidth", 8.5 * t2.in)
+  t2.set("paperheight", 11 * t2.in)
+  t2.add("This is Letter size")
+  t2.end()
+  t2.destroy()
